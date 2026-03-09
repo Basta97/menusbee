424c1a57c4bf7bf0b2930c064733a33f511d38c2
ahmedBasta <ahmed.b.01.12.97@gmail.com>
2026-03-09 14:31:35 +0200

    fix

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -8,5 +8,20 @@ import { QrCto } from "../../shared/common/qr-cto/qr-cto";
   styleUrl: './footer.css',
 })
 export class Footer {
+  isChatOpen: boolean = false;
 
+  toggleChat(event?: Event) {
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    this.isChatOpen = !this.isChatOpen;
+  }
+
+  sendMessage(message: string) {
+    if (!message.trim()) return;
+    const url = `https://wa.me/2010104099525?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+    this.isChatOpen = false;
+  }
 }
